6c2ec9a93a19828fe3ab523150470285a225445c
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-08-30 15:58:25 +0530

    Components

---
+++ Day-2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Classcom from './Component/Classcom'
+
+export default function App() {
+  return (
+    <div>
+      <Classcom/>
+    </div>
+  )
+}
---
+++ Day-2/src/Component/Functiomcom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Functiomcom() {
+  return (
+    <div>
+      app
+    </div>
+  )
+}
