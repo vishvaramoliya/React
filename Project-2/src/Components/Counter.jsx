eeea870a40926b7535a7f0f5d73766ca5a28938a
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-09-04 15:32:56 +0530

    Project-2

---
+++ Project-2/src/Components/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+
+    const [count,setCount] = useState(0)
+    let c = 0;
+   
+  return (
+
+    <>
+        <h1>Counter</h1>
+        <p>{count}</p>
+        <button className='btn-12' onClick={()=>setCount(count + 1)}>
+            <span>+</span> <span>Increment</span>
+        </button>
+        <button className='btn-12' onClick={()=>setCount(0)}>
+        <span>Reset</span> <span>Reset</span>
+        </button>
+        <button className='btn-12' onClick={()=>setCount(count - 1)}>
+        <span>-</span> <span>Decrement</span>
+        </button>
+
+    
+    </>
+)
+}
