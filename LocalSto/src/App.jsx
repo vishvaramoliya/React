9638da62b6b3a0952c57f0f00b229d0dfdd73a2c
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-09-23 15:49:18 +0530

    LocalSto

---
+++ LocalSto/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import First from './Components/first'
+import Sec from './Components/Sec'
+import Third from './Components/Third'
+
+export default function App() {
+  return (
+    <div>
+        <First/>
+        <Sec/>
+        <Third/>
+    </div>
+  )
+}
---
+++ LocalSto/src/Components/Third.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Third() {
+    
+    localStorage.setItem("name" , "Vishwa");
+    localStorage.setItem("name1" , "Priya");
+
+    let data = localStorage.getItem("name")
+    console.log(data);
+
+    let obj = {name : "Vishwa" , subject : "React"}
+    localStorage.setItem("myOBJ" , JSON.stringify(obj));
+
+    let OBJData =  JSON.parse(localStorage.getItem("myOBJ"))
+    
+    console.log(OBJData);
+    
+
+  return (
+    <div>first</div>
+  )
+}
