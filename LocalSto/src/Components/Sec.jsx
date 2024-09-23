476094b5a3075efb04be3b252c82a0a63234d667
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-09-23 21:32:03 +0530

    Localsto

--- LocalSto/src/Components/Sec.jsx
+++ LocalSto/src/Components/Sec.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 
 export default function Sec() {
-    
-    localStorage.setItem("name" , "Vishwa");
-    localStorage.setItem("name1" , "Priya");
 
-    let data = localStorage.getItem("name")
-    console.log(data);
+  localStorage.setItem("name", "vishwa")
 
-    let obj = {name : "Vishwa" , subject : "React"}
-    localStorage.setItem("myOBJ" , JSON.stringify(obj));
+  let data = localStorage.getItem("name")
+  console.log(data)
 
-    let OBJData =  JSON.parse(localStorage.getItem("myOBJ"))
-    
-    console.log(OBJData);
-    
+  let obj = { name: "VISHWA", sername: "RAMOLIYA" }
+  localStorage.setItem("myObj", JSON.stringify(obj))
+
+  let objdata = JSON.parse(localStorage.getItem("myObj"))
+  console.log(objdata);
 
   return (
-    <div>first</div>
+    <div>Sec</div>
   )
 }
