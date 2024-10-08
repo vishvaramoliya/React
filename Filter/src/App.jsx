a3f7c5ffc6c5c726382457c1524055acf9c12d8b
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-10-08 12:47:00 +0530

    Filter

---
+++ Filter/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Filter from './Components/Filter'
+
+export default function App() {
+  return (
+    <div>
+      <Filter/>
+    </div>
+  )
+}
---
+++ Filter/src/Components/Filter.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export default function Filter() {
+
+    const [name, setName] = useState("")
+    const arr = [
+        { name: "Vishwa" , subject : "React" },
+        { name: "Priya"  , subject: "Nodejs"},
+        { name: "Rutul"  , subject: "Javascript"},
+        { name: "khushal"  , subject: "React"},
+        { name: "Saurabh"  , subject: "Bootstrap"}
+    ]
+
+    const data = arr.filter((el)=>el.name.includes(name));
+    
+    return (
+        <div>
+            <h1>Filter</h1>
+            <input type="text" placeholder='Enter...' onChange={(e)=>setName(e.target.value)}/>
+            {
+                data && 
+                data.map((e,i)=>{
+                    return <ul>
+                        <li>{e.name}</li>
+                        <li>{e.subject}</li>
+                    </ul>
+                })
+            }
+        </div>
+    )
+}
