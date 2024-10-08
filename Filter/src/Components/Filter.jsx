5c759ada075501270b501e4911ca94e023ece7c2
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-10-08 13:03:00 +0530

    Filter

--- Filter/src/Components/Filter.jsx
+++ Filter/src/Components/Filter.jsx
@@ -4,25 +4,26 @@ export default function Filter() {
 
     const [name, setName] = useState("")
     const arr = [
-        { name: "Vishwa" , subject : "React" },
-        { name: "Priya"  , subject: "Nodejs"},
-        { name: "Rutul"  , subject: "Javascript"},
-        { name: "khushal"  , subject: "React"},
-        { name: "Saurabh"  , subject: "Bootstrap"}
+        { name: "Vishwa", subject: "React" },
+        { name: "Priya", subject: "Nodejs" },
+        { name: "Rutul", subject: "Javascript" },
+        { name: "Ayush", subject: "Tailwind" },
+        { name: "khushal", subject: "React" },
+        { name: "Saurabh", subject: "Bootstrap" }
     ]
 
-    const data = arr.filter((el)=>el.name.includes(name));
-    
+    const data = arr.filter((el) => el.name.includes(name));
+
     return (
         <div>
             <h1>Filter</h1>
-            <input type="text" placeholder='Enter...' onChange={(e)=>setName(e.target.value)}/>
+            <input type="text" placeholder='Enter...' onChange={(e) => setName(e.target.value)} />
             {
-                data && 
-                data.map((e,i)=>{
+                data &&
+                data.map((e, i) => {
                     return <ul>
                         <li>{e.name}</li>
-                        <li>{e.subject}</li>
+                        <li>{e.name}</li>
                     </ul>
                 })
             }
