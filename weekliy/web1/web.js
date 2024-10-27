29f6a0b468caa30e437299fb853f8842a0ef8490
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-10-27 18:23:55 +0530

    week1

--- weekliy/web1/web.js
+++ weekliy/web1/web.js
@@ -1,12 +1,30 @@
 window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
     var text = document.getElementById('a');
-
-    if (window.scrollY >= window.innerHeight * 0.8) {
-      header.style.backgroundColor = 'black';
+    var text1 = document.getElementById('nav')
+    if (window.scrollY >= window.innerHeight * 0.1  ) {
+      header.style.backgroundColor = '#101010';
+      header.style.boxShadow = 'rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px';
+      header.style.boxBottom = '1px solid white';
       text.style.color = 'red';
+      header.style.height = '70px';
     } else {
       header.style.backgroundColor = 'transparent';
       text.style.color = 'white'
+      header.style.height = '100px';
     }
   });
+
+  const icon = document.getElementById('icon1');
+  const nav = document.getElementById('nav');
+
+  icon.addEventListener('click', () => {
+      if (nav.style.display === 'block') {
+          nav.style.display = 'none';
+          
+      } else {
+          nav.style.display = 'block';
+      }
+  });
+
+  nav.style.display = 'none';
